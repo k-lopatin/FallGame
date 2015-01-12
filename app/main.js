a30ee7acde59dbb710284a0dc5790446ff394015
lk1195 <lk1195@ya.ru>
2015-01-12 22:26:08 +0300

    0.0.1

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
 /**
  * Created by Константин on 12.01.2015.
  */
-var game = new Phaser.Game(400, 640, Phaser.AUTO, '', { preload: preload, create: create });
+var game = new Phaser.Game(400, 640, Phaser.AUTO, '', {preload: preload, create: create, update: update});
 
 function preload() {
 
@@ -10,10 +10,16 @@ function preload() {
 
 }
 
+var blocks = [];
+var balls = [];
+var helperSprites = [];
+
 function setBlocks() {
-    for(var i=0; i<400; i+=67){
+
+    for (var i = 0; i < 400; i += 67) {
         var block = game.add.sprite(i, 610, 'block');
-        console.log(game.world.height);
+        game.physics.enable(block, Phaser.Physics.ARCADE);
+        blocks.push(block);
     }
 }
 
@@ -22,13 +28,25 @@ function create() {
     setBlocks();
 }
 
-function getRandomInt(min, max)
-{
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+function update() {
+    //console.log(bottom);
+    for(var i=0; i<balls.length; i++){
+        if(balls[i].y > 590){
+            alert('Game is Finished');
+            game.destroy();
+        }
+        for(var j=0; j<blocks.length; j++){
+            game.physics.arcade.collide(balls[i], blocks[j], blockCollide, null, this);
+        }
+    }
 }
 
-setInterval( function() {
-    var ball = game.add.sprite(getRandomInt(0, 355), 0, 'ball');
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+setInterval(function () {
+    var ball = game.add.sprite(getRandomInt(0, 355), -45, 'ball');
     game.physics.enable(ball, Phaser.Physics.ARCADE);
     ball.body.velocity.x = 0;
     ball.body.velocity.y = getRandomInt(50, 300);
@@ -36,11 +54,23 @@ setInterval( function() {
     ball.body.collideWorldBounds = true;
     ball.body.bounce.setTo(1, 1);
     ball.events.onInputDown.add(ballClicked, ball);
+
+    balls.push(ball);
+
 }, 400)
 
-function ballClicked(){
+function ballClicked() {
     this.kill();
 }
 
+function blockCollide(ball, block) {
+    console.log('test');
+    ball.kill();
+    block.kill();
+}
+
+function bottomCollide(ball, block) {
+    console.log('finished');
+}
 
 
