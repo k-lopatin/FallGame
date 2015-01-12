e9bdd311736cf82804c5d2f44399a6bc48fc548d
lk1195 <lk1195@ya.ru>
2015-01-12 21:11:04 +0300

    blocks added

--- app/main.js
+++ app/main.js
@@ -5,17 +5,21 @@ var game = new Phaser.Game(400, 640, Phaser.AUTO, '', { preload: preload, create
 
 function preload() {
 
-    //  You can fill the preloader with as many assets as your game requires
-
-    //  Here we are loading an image. The first parameter is the unique
-    //  string by which we'll identify the image later in our code.
-
-    //  The second parameter is the URL of the image (relative)
     game.load.image('ball', '../assets/img/ellipse.png');
+    game.load.image('block', '../assets/img/polygon.png');
+
+}
+
+function setBlocks() {
+    for(var i=0; i<400; i+=67){
+        var block = game.add.sprite(i, 610, 'block');
+        console.log(game.world.height);
+    }
 }
 
 function create() {
-
+    game.stage.backgroundColor = '#f4f4e8';
+    setBlocks();
 }
 
 function getRandomInt(min, max)
@@ -24,7 +28,7 @@ function getRandomInt(min, max)
 }
 
 setInterval( function() {
-    var ball = game.add.sprite(getRandomInt(0, 340), 0, 'ball');
+    var ball = game.add.sprite(getRandomInt(0, 355), 0, 'ball');
     game.physics.enable(ball, Phaser.Physics.ARCADE);
     ball.body.velocity.x = 0;
     ball.body.velocity.y = getRandomInt(50, 300);
@@ -35,3 +39,6 @@ setInterval( function() {
 function ballClicked(){
     this.kill();
 }
+
+
+
