e252021964c0e6cf6af8cd3b81a788dc8a4e1680
Rostislav <shtanko243@gmail.com>
2015-01-13 17:35:22 +0300

    Little changes

--- app/main.js
+++ app/main.js
@@ -140,20 +140,22 @@ setInterval(function () {
 
 
 setInterval(function(){
-    var x = getRandomInt(0, GAME_WIDTH);
+    var x = getRandomInt(60, GAME_WIDTH-60);
     var y = getRandomInt(0, maxBlockY-60);
     var timestop = game.add.sprite(x, y, 'stop_balls');
-
     timestop.inputEnabled = true;
     timestop.events.onInputDown.add(timeStopClicked, timestop);
-    setTimeout(function(){
-        killTimeStop(timestop);
-        points -= 2;
-        pointsText.text = ''+points;
-    }, 2500)
+    killTimeStopStatic(timestop);
 
 }, 5500)
 
+function killTimeStopStatic(t) {
+    setTimeout(function () {
+        killTimeStop(t);
+        points -= 2;
+        pointsText.text = '' + points;
+    }, 2500)
+}
 
 function ballClicked() {
     killBall(this);
@@ -165,12 +167,7 @@ function fallingBlockClicked() {
     points -= 2;
     pointsText.text = ''+points;
 }
-function timeStopClicked() {
-    for(var i=0; i<balls.length; i++){
-        balls[i].body.velocity.x = 0;
-        balls[i].body.velocity.y = 0;
-    }
-    killTimeStop(this);
+function StartBallsMove(){
     setTimeout(function(){
         for(var i=0; i<balls.length; i++){
             switch(balls[i].key)
@@ -187,6 +184,15 @@ function timeStopClicked() {
 
         }
     }, 2500)
+}
+
+function timeStopClicked() {
+    for(var i=0; i<balls.length; i++){
+        balls[i].body.velocity.x = 0;
+        balls[i].body.velocity.y = 0;
+    }
+    killTimeStop(this);
+    StartBallsMove();
 }
 
 function blockCollide(ball, block) {
